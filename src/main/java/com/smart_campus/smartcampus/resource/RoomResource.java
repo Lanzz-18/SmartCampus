1fc6c0f51289014f31ff1f0a2ad10e7d180dd806
Senal Perera <senalperera777@gmail.com>
2026-04-22 06:00:56 +0530

    feat: add logging filter and RoomResource

---
+++ src/main/java/com/smart_campus/smartcampus/resource/RoomResource.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smart_campus.smartcampus.resource;
+
+/**
+ *
+ * @author senal
+ */
+
+import com.smart_campus.smartcampus.dao.Datastore;
+import com.smart_campus.smartcampus.exception.RoomNotEmptyException;
+import com.smart_campus.smartcampus.model.Room;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/rooms")
+public class RoomResource {
+    // Returning all the rooms in the datastore
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllRooms(){
+        List<Room> roomList = new ArrayList<>(Datastore.rooms.values());
+        return Response.ok(roomList).build();
+    }
+    
+    // Creating a new room from JSON body
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createRoom(Room room){
+        Datastore.rooms.put(room.getId(), room);
+        return Response.status(201).entity(room).build();
+    }
+    
+    // Returning a single room by ID
+    @GET
+    @Path("/{roomId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getRoomById(@PathParam("roomId") String roomId) {
+        Room room = Datastore.rooms.get(roomId);
+        if(room == null) {
+            return Response.status(404).entity("Room not found").build();
+        }
+        return Response.ok(room).build();
+    }
+    
+    //Deleting a room, if it has no sensors aligned
+    @DELETE
+    @Path("/{roomId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deletResponse(@PathParam("roomId") String roomId) {
+        Room room = Datastore.rooms.get(roomId);
+        if (room == null) {
+            return Response.status(404).entity("Room not found").build();
+        }
+        if (!room.getSensorIds().isEmpty()) {
+            throw new RoomNotEmptyException("Room " + roomId + " cannot be deleted due to still having sensors assigned");
+        }
+        Datastore.rooms.remove(roomId);
+        return Response.noContent().build();
+    }
+}
